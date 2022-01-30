b00dab8fb46036ad0e2c11ceffdf167b31720974
Tomas Knapen <tknapen@gmail.com>
2022-01-30 14:45:49 +0100

    first attempt at setting things up

---
+++ popylar/rf.py
@@ -0,0 +1,145 @@
+import numpy as np
+import scipy.stats as stats
+
+def gauss1D_cart(x, mu=0.0, sigma=1.0):
+    """gauss1D_cart
+
+    gauss1D_cart takes a 1D array x, a mean and standard deviation,
+    and produces a gaussian with given parameters, as a pdf.
+
+    Parameters
+    ----------
+    x : numpy.ndarray (1D)
+        space on which to calculate the gauss
+    mu : float, optional
+        mean/mode of gaussian (the default is 0.0)
+    sigma : float, optional
+        standard deviation of gaussian (the default is 1.0)
+
+    Returns
+    -------
+    numpy.ndarray
+        gaussian values at x
+    """
+
+    return stats.norm.pdf(x, loc=mu, scale=sigma)
+
+def vonMises1D(x, mu=0.0, kappa=1.0):
+    """vonMises1D
+
+    vonMises1D takes a 1D array x, a mean and kappa (inverse of standard deviation),
+    and produces a von Mises pRF with given parameters. This shape can be thought of 
+    as a circular gaussian shape. Used for orientation or motion direction pRFs, 
+    for instance.
+
+    Parameters
+    ----------
+    x : numpy.ndarray (1D)
+        space on which to calculate the von Mises. 
+        Assumed to be in the range (0, 2*np.pi)
+    mu : float, optional
+        mean/mode of von Mises (the default is 0.0)
+    kappa : float, optional
+        dispersion coefficient of the von Mises, 
+        akin to inverse of standard deviation of gaussian (the default is 1.0)
+
+    Returns
+    -------
+    numpy.ndarray
+        von Mises values at x, peak has y-value of 1
+    """
+    return stats.vonmises.pdf(x, kappa=kappa, loc=mu)
+
+def gauss2D_iso_cart(x, y, mu=(0.0, 0.0), sigma=1.0):
+    """gauss2D_iso_cart
+
+    gauss2D_iso_cart takes two-dimensional arrays x and y, containing
+    the x and y coordinates at which to evaluate the 2D isotropic gaussian 
+    function, with a given sigma, and returns a 2D array of Z values.
+
+    Parameters
+    ----------
+    x : numpy.ndarray, 2D or flattened by masking
+        2D, containing x coordinates
+    y : numpy.ndarray, 2D or flattened by masking
+        2D, containing y coordinates
+    mu : tuple, optional
+        mean, 2D coordinates of mean/mode of gauss (the default is (0.0,0.0))
+    sigma : float, optional
+        standard deviation of gauss (the default is 1.0)
+
+    Returns 
+    -------
+    numpy.ndarray, 2D or flattened by masking
+        gaussian values evaluated at (x,y)
+    """
+    return np.exp(-((x-mu[0])**2 + (y-mu[1])**2)/(2*sigma**2)) / (2*np.pi*sigma**2)
+
+def gauss2D_rot_cart(x, y, mu=(0.0, 0.0), sigma=1.0, theta=0.0, ar=1.0):
+    """gauss2D_rot_cart
+
+    gauss2D_rot_cart takes two-dimensional arrays x and y, containing
+    the x and y coordinates at which to evaluate the 2D non-isotropic gaussian 
+    function, with a given sigma, angle of rotation theta, and aspect ratio ar.
+    it returns a 2D array of Z values. Default is an isotropic gauss.
+
+    Parameters
+    ----------
+    x : numpy.ndarray, 2D
+        2D, containing x coordinates or flattened by masking
+    y : numpy.ndarray, 2D
+        2D, containing y coordinates or flattened by masking
+    mu : tuple, optional
+        mean, 2D coordinates of mean/mode of gauss (the default is (0.0,0.0))
+    sigma : float, optional
+        standard deviation of gauss (the default is 1.0)
+    theta : float, optional
+        angle of rotation of gauss (the default is 0.0)   
+    ar : float, optional
+        aspect ratio of gauss, multiplies the rotated y parameters (the default is 1.0)
+
+    Returns
+    -------
+    numpy.ndarray, 2D or flattened by masking
+        gaussian values evaluated at (x,y) 
+    """
+    xr = (x-mu[0]) * np.cos(theta) + (y-mu[1]) * np.sin(theta)
+    yr = -(x-mu[0]) * np.sin(theta) + (y-mu[1]) * np.cos(theta)
+
+    return np.exp(-(xr**2 + ar**2 * yr**2)/(2*sigma**2))
+
+def gauss2D_logpolar(ecc, polar, mu=(1.0, 0.0), sigma=1.0, kappa=1.0):
+    """gauss2D_logpolar
+
+    gauss2D_logpolar takes two-dimensional arrays ecc and polar, containing
+    the eccentricity and polar angle coordinates at which to evaluate the 2D gaussian, 
+    which in this case is a von Mises in the polar angle direction, and a log gauss 
+    in the eccentricity dimension, and returns a 2D array of Z values.
+    We recommend entering the ecc and polar angles ordered as x and y for easy
+    visualization.
+
+    Parameters
+    ----------
+    ecc : numpy.ndarray, 2D or flattened by masking
+        2D, containing eccentricity
+    polar : numpy.ndarray, 2D or flattened by masking
+        2D, containing polar angle coordinates (0, 2*np.pi)
+    mu : tuple, optional
+        mean, 2D coordinates of mean/mode of gauss (ecc) and von Mises (polar) (the default is (0.0,0.0))
+    sigma : float, optional
+        standard deviation of gauss (the default is 1.0)
+    kappa : float, optional
+        dispersion coefficient of the von Mises, 
+        akin to inverse of standard deviation of gaussian (the default is 1.0)
+
+    Returns
+    -------
+    numpy.ndarray, 2D or flattened by masking
+        values evaluated at (ecc, polar), peak has y-value of 1.
+    """
+    ecc_gauss = np.exp(-(np.log(ecc/mu[0])**2)/(2*sigma**2))
+    polar_von_mises = stats.vonmises.pdf(polar-mu[1], kappa)
+    polar_von_mises /= np.max(polar_von_mises)
+    logpolar_Z = ecc_gauss * polar_von_mises
+
+    return logpolar_Z / np.max(logpolar_Z)
---
+++ popylar/stimuli/stimulus.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+class Stimulus:
+    """Stimulus
+
+    Minimal pRF stimulus class, base of others
+    Design matrix has final dimension time.
+
+    """
+
+    def __init__(self,
+                 design_matrix,
+                 sample_rate,                 
+                 coordinates,
+                 **kwargs):
+        """__init__ initialize Stimulus
+
+        Parameters
+        ----------
+        design_matrix : numpy.ndarray
+            numpy array containing the design matrix, in [horizontal, vertical, time] dimensions
+        sample_rate : float
+            amount of samples per second
+        coordinates : numpy.ndarray
+            coordinates for the 'pixels' in each of the 'feature' dimensions of the design matrix
+        """
+        self.coordinates = coordinates
+        self.design_matrix = design_matrix
+        self.sample_rate = sample_rate
+        self.__dict__.update(kwargs)
+        self.mask_dm()
+
+    def mask_dm(self):
+        """mask_dm sets up coordinates such that there are no coordinates for design matrix elements that are empty
+        which also converts the design matrix to a 1D array for ease of implementation later 
+        """
+        self.dm_mask = self.design_matrix.sum(-1) != 0
+        self.masked_design_matrix = self.design_matrix[self.dm_mask]
+        self.masked_coordinates = [c[self.dm_mask] for c in self.coordinates]
+
+class PRFStimulus1D(Stimulus):
+    """PRFStimulus1D 
+
+    Minimal 1-dimensional pRF stimulus class, 
+    which takes an input design matrix and sets up its real-world dimensions.
+
+    this type of stimulus could be an auditory pRF stimulus, a numerosity stimulus,
+    or a connective field stimulus
+
+    """
+    def __init__(self,
+                design_matrix, 
+                sample_rate, 
+                coordinates,
+                **kwargs):
+        """__init__ initialize PRFStimulus1D
+
+        Parameters
+        ----------
+        design_matrix : numpy.ndarray
+            numpy array containing the design matrix, in 
+            {frequency, log(frequency), numerosity, log(numerosity), distance} dimensions
+        sample_rate : float
+            amount of samples per second
+        coordinates : numpy.ndarray
+            [description]
+        """
+
+class PRFStimulus2D(Stimulus):
+    """PRFStimulus2D
+
+    Minimal visual 2-dimensional pRF stimulus class, 
+    which takes an input design matrix and sets up its real-world dimensions.
+
+    """ 
+    def __init__(self,
+                 design_matrix,
+                 sample_rate,                 
+                 screen_size_cm,
+                 screen_distance_cm,
+                 **kwargs):
+        """__init__ initialize PRFStimulus2D
+
+        Parameters
+        ----------
+        screen_size_cm : float
+            the size of the screen in cms. This refers to the screen width, the first dimension of the design matrix.
+        screen_distance_cm : float
+            the distance from eye to screen in cms.
+        design_matrix : numpy.ndarray
+            numpy array containing the design matrix, in [horizontal, vertical, time] dimensions
+        sample_rate : float
+            amount of samples per second
+        """
+        self.screen_size_cm = screen_size_cm
+        self.screen_distance_cm = screen_distance_cm
+        self.screen_size_degrees = 2.0 * \
+            np.degrees(np.arctan(self.screen_size_cm /
+                                 (2.0*self.screen_distance_cm)))
+        width_coordinates = np.linspace(-self.screen_size_degrees/2,
+                                self.screen_size_degrees/2,
+                                design_matrix.shape[0],
+                                endpoint=True)
+        hw_ratio = design_matrix.shape[1]/design_matrix.shape[0]
+        height_coordinates = np.linspace(-hw_ratio*self.screen_size_degrees/2,
+                                hw_ratio*self.screen_size_degrees/2,
+                                design_matrix.shape[1],
+                                endpoint=True)
+        self.x, self.y = np.meshgrid(width_coordinates, height_coordinates)
+        super().__init__(coordinates=[self.x, self.y],
+                        design_matrix=design_matrix,
+                        sample_rate=sample_rate, 
+                        kwargs=kwargs)
+
+
+
+
